9306cf3ff4e1ad17fb4aaba1fd53c704c6e23912
江超群 <mcoder2014@sina.com>
2017-08-28 11:59:53 +0800

    表格的总体大框架

--- ofdEditor/model/Doc/DocTable.cpp
+++ ofdEditor/model/Doc/DocTable.cpp
@@ -17,7 +17,7 @@ DocTable::DocTable(QWidget *parent)
     this->_table = cursor.insertTable(3,4);         // 插入一个表格
 
     this->init();
-    this->initConnection();
+
 
 }
 
@@ -34,8 +34,6 @@ DocTable::DocTable(int rows, int columns)
     this->_table = cursor.insertTable(rows, columns);         // 插入一个表格
 
     this->init();
-    this->initConnection();
-
 }
 
 
@@ -88,6 +86,27 @@ QMenu *DocTable::getMenu()
         this->actionPaste->setEnabled(true);
     }
 
+    // 选择表格的状态
+    QTextCursor cursor = this->textCursor();            // 文本光标
+//    int firstRow, numRows, firstColumn, numColumns;     // 选择的
+//    cursor.selectedTableCells(
+//                &firstRow, &numRows, &firstColumn, &numColumns);
+    this->selectedCells(cursor);
+
+    qDebug() << "firstRow, numRows, firstColumn, numColumns"
+             << this->firstRow
+             << this->rows
+             << this->firstColumn
+             << this->columns;
+
+    this->ContextMenu->addSeparator();      // ------------------
+    this->ContextMenu->addAction(this->actionTableSetting); // 表格设置
+    this->ContextMenu->addMenu(this->menu_insertRowOrCol);  // 插入行或列
+    this->ContextMenu->addAction(this->actionMergeCells);   // 合并单元格
+    this->ContextMenu->addAction(this->actionSplitCells);   // 拆分单元格
+    this->ContextMenu->addAction(this->actionDeleteRow);    // 删除该行
+    this->ContextMenu->addAction(this->actionDeleteColumn); // 删除该列
+
     return this->ContextMenu;
 }
 
@@ -139,7 +158,96 @@ void DocTable::setBlock(DocBlock *block)
     this->block = block;
     this->connect(block, SIGNAL(signal_resize(qreal,qreal,qreal,qreal)),
                   this, SLOT(blockSizeChanged()));
-//    qDebug() << "set block success table";
+    //    qDebug() << "set block success table";
+}
+
+///
+/// \brief DocTable::insertRowBefore
+///     在之前插入行
+///
+void DocTable::insertRowBefore()
+{
+    QTextCursor cursor = this->textCursor();
+    this->insertRow(cursor, -1);
+}
+
+///
+/// \brief DocTable::insertRow
+///     插入行
+/// \param cursor
+/// \param flag
+///
+void DocTable::insertRow(QTextCursor cursor, int flag)
+{
+    int firstRow, numRows, firstColumn, numColumns;     // 选择的
+    cursor.selectedTableCells(
+                &firstRow, &numRows, &firstColumn, &numColumns);
+
+    // 插入行
+    if(flag == -1)
+    {
+        // 在选择区域最左侧插入一行
+
+    }
+    else if(flag == 1)
+    {
+
+    }
+
+}
+
+void DocTable::insertRowAfter()
+{
+
+}
+
+void DocTable::insertColBefore()
+{
+
+}
+
+void DocTable::insertColAfter()
+{
+
+}
+
+void DocTable::mergeCells()
+{
+    QTextCursor cursor = this->textCursor();
+    this->_table->mergeCells(cursor);
+}
+
+void DocTable::splitCells()
+{
+
+}
+
+void DocTable::delRow()
+{
+
+}
+
+void DocTable::delCol()
+{
+
+}
+
+void DocTable::tableSetting()
+{
+
+}
+
+void DocTable::updateStyle()
+{
+
+    if(this->isDefaultStyle)
+    {
+        this->setDefaultStyle();
+    }
+    else
+    {
+        // 按照设置的比例调节
+    }
 }
 
 void DocTable::focusInEvent(QFocusEvent *e)
@@ -153,19 +261,66 @@ void DocTable::focusOutEvent(QFocusEvent *e)
     QTextEdit::focusOutEvent(e);
 }
 
+void DocTable::selectedCells(QTextCursor &cursor)
+{
+    // 这里可以检测是否复选单元格
+    cursor.selectedTableCells(
+                &this->firstRow,
+                &this->rows,
+                &this->firstColumn,
+                &this->columns);
+
+    if(
+            this->firstRow == -1
+            && this->rows == -1
+            && this->firstColumn == -1
+            && this->columns == -1)
+    {
+        QTextTableCell cell = this->_table->cellAt(cursor);
+        this->firstRow = cell.row();
+        this->rows = cell.rowSpan();
+        this->firstColumn = cell.column();
+        this->columns = cell.columnSpan();
+    }
+
+}
+
 void DocTable::init()
 {
 //    this->setFrameStyle(QFrame::NoFrame);
     this->setMinimumSize(50,10);
 
-//    // 关闭滚动条
-//    this->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
-//    this->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
-//    // 设置为背景透明
-//    this->viewport()->setAttribute(Qt::WA_TranslucentBackground, true);
-    this->setDefaultStyle();            // 设置默认样式
+    this->firstRow = 0;
+    this->firstColumn = 0;
+    this->rows = 1;
+    this->columns = 1;
 
+    this->isDefaultStyle = true;
+    this->updateStyle();            // 更新样式
+
+    this->initAction();
     this->initMenu();
+    this->initConnection();
+
+}
+
+///
+/// \brief DocTable::initAction
+///     初始化QAction
+void DocTable::initAction()
+{
+    // 插入
+    this->actionInsertColAfter = new QAction(tr("insert Col After"), this);
+    this->actionInsertColBefore = new QAction(tr("insert Col Before"), this);
+    this->actionInsertRowAfter = new QAction(tr("insert row after"), this);
+    this->actionInsertRowBefore = new QAction(tr("insert row before"), this);
+
+    this->actionMergeCells = new QAction(tr("Merge Cells"), this);      // 合并单元格
+    this->actionSplitCells = new QAction(tr("Split Cells"), this);      // 拆分单元格
+
+    this->actionDeleteColumn = new QAction(tr("delete column"), this);  // 删除列
+    this->actionDeleteRow = new QAction(tr("delete row"), this);        // 删除整行
+    this->actionTableSetting = new QAction(tr("table setting"), this);  // 表格设置
 
 }
 
@@ -188,12 +343,63 @@ void DocTable::initMenu()
     this->ContextMenu->addAction(this->actionFontSetTest);  // 字体
     this->ContextMenu->addAction(this->actionParagraph);    // 段落设置
 
-    qDebug() << "init Menu" <<this->ContextMenu;
+    this->ContextMenu->addSeparator();                      // -------------
+    this->ContextMenu->addAction(this->actionTableSetting); // 表格设置
+
+    this->menu_insertRowOrCol = new QMenu(tr("Insert Row or Col"));     // 插入行或列
+    this->menu_insertRowOrCol->addAction(this->actionInsertRowBefore);
+    this->menu_insertRowOrCol->addAction(this->actionInsertRowAfter);
+    this->menu_insertRowOrCol->addAction(this->actionInsertColBefore);
+    this->menu_insertRowOrCol->addAction(this->actionInsertColAfter);
+
+    this->ContextMenu->addMenu(this->menu_insertRowOrCol);
+    this->ContextMenu->addAction(this->actionMergeCells);   // 合并单元格
+    this->ContextMenu->addAction(this->actionSplitCells);   // 拆分单元格
+    this->ContextMenu->addAction(this->actionDeleteRow);    // 删除该行
+    this->ContextMenu->addAction(this->actionDeleteColumn); // 删除该列
 
 }
 
 void DocTable::initConnection()
 {
+
+    // 在当前单元格之前插入列
+    this->connect(this->actionInsertColBefore, SIGNAL(triggered(bool)),
+                  this, SLOT(insertColBefore()));
+
+    // 在当前单元格之后插入列
+    this->connect(this->actionInsertColAfter, SIGNAL(triggered(bool)),
+                  this, SLOT(insertColAfter()));
+
+    // 在当前单元格之前插入行
+    this->connect(this->actionInsertRowBefore, SIGNAL(triggered(bool)),
+                  this, SLOT(insertRowBefore()));
+
+    // 在当前单元格之后插入
+    this->connect(this->actionInsertRowAfter, SIGNAL(triggered(bool)),
+                  this, SLOT(insertRowAfter()));
+
+    // 合并单元格
+    this->connect(this->actionMergeCells, SIGNAL(triggered(bool)),
+                  this, SLOT(mergeCells()));
+
+    // 拆分单元格-------我觉得拆分单元格要慎重-就只把原本合并的单元格分开就好了？
+    this->connect(this->actionSplitCells, SIGNAL(triggered(bool)),
+                  this, SLOT(splitCells()));
+
+    // 删除整行
+    this->connect(this->actionDeleteRow, SIGNAL(triggered(bool)),
+                  this, SLOT(delRow()));
+
+    // 删除整列
+    this->connect(this->actionDeleteColumn, SIGNAL(triggered(bool)),
+                  this, SLOT(delCol()));
+
+    // 表格设置
+    this->connect(this->actionTableSetting, SIGNAL(triggered(bool)),
+                    this, SLOT(tableSetting()));
+
+    // 自动调整大小
     this->connect(this, SIGNAL(textChanged()),
                   this, SLOT(blockSizeChanged()));
 }
--- ofdEditor/model/Doc/DocTable.h
+++ ofdEditor/model/Doc/DocTable.h
@@ -46,24 +46,65 @@ public slots:
     void setDefaultStyle();                      // 设置默认样式
     void setBlock(DocBlock* block);              // 设置块
 
+    // 插入行或列
+    void insertRowBefore();
+    void insertRow(
+            QTextCursor cursor,
+            int flag);
+    void insertRowAfter();
+    void insertColBefore();
+    void insertColAfter();
+
+    // 合并单元格
+    void mergeCells();
+
+    // 拆分单元格
+    void splitCells();
+
+    // 删除
+    void delRow();
+    void delCol();
+
+    // 单元格设置
+    void tableSetting();
+    void updateStyle();     // 更新表格的样式
+
 protected:
     void focusInEvent(QFocusEvent *e);
     void focusOutEvent(QFocusEvent *e);
 
 private slots:
+    void selectedCells(QTextCursor& cursor);
 
 private:
     int rowCount;               // 行数
     int colCount;               // 列数
     QTextTable* _table;         // 该表格
+    QVector<QTextLength> colWidth;  // 列宽设置
+    bool isDefaultStyle;
+
+    // 选择的结果
+    int firstRow;
+    int firstColumn;
+    int rows;
+    int columns;
 
     void init();                // 通用的初始化部分
+    void initAction();          // 初始化Action
     void initMenu();            // 初始化菜单
     void initConnection();      // 初始化信号槽链接
 
+    QMenu *menu_insertRowOrCol;     // 插入行或列
+    QAction *actionInsertRowBefore; // 在当前单元格之前插入行
+    QAction *actionInsertRowAfter;  // 在当前单元格之后插入行
+    QAction *actionInsertColBefore; // 在当前单元格之前插入列
+    QAction *actionInsertColAfter;  // 在当前单元格之后插入列
+
+    QAction *actionMergeCells;      // 合并单元格
+    QAction *actionSplitCells;      // 拆分单元格
+
     QAction *actionDeleteRow;       // 删除该行
     QAction *actionDeleteColumn;    // 删除该列
-    QAction *actionInsertRowOrCol;  // 插入行或列-由对话框解决
     QAction *actionTableSetting;    // 表格设置-对话框解决
 
 private slots:
